279c5b233e4d4969ce6c859deef02216ed94f0db
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-06-08 15:01:57 +0700

    Update BE URL

--- src/utils/AppConfig.js
+++ src/utils/AppConfig.js
@@ -1,3 +1,3 @@
-export const BACK_END_HOST = "http://localhost:3008/api/v1";
+export const BACK_END_HOST = "https://bepsinhvien-be.vercel.app/api/v1";
 export const FRONT_END_HOST = "http://localhost:5173/";
 // export const BACK_END_HOST_QUANG = "http://localhost:3008";
